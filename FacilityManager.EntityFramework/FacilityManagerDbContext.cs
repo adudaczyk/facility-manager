5dd15d7ad2f7b6896fa0299678c7366822121104
Adrian Dudaczyk <a.dudaczyk@gmail.com>
2020-11-26 19:49:52 +0100

    Add user to dbcontext

--- FacilityManager.EntityFramework/FacilityManagerDbContext.cs
+++ FacilityManager.EntityFramework/FacilityManagerDbContext.cs
@@ -5,6 +5,7 @@ namespace FacilityManager.EntityFramework
 {
     public class FacilityManagerDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
         public DbSet<Facility> Facilities { get; set; }
 
         public FacilityManagerDbContext(DbContextOptions<FacilityManagerDbContext> options)
